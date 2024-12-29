3274a1ae8efa63b4f905b0add956942b2279ceb9
abhay desai <abhaydesai428@gmail.com>
2024-12-29 20:41:17 +0530

    testing 1

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Dashboard from './components/Dashboard.jsx'
+import SignIN from './components/registration.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Dashboard />
+    <SignIN />
   </StrictMode>,
 )
